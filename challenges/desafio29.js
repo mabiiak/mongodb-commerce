774efe6c63ab98653b5a70fdfc6eec17c8044072
mabiiak <mabiane.polniak@gmail.com>
2022-07-07 21:16:41 -0300

    fix: complete querys 29, 30

--- challenges/desafio29.js
+++ challenges/desafio29.js
@@ -1,3 +1,5 @@
-// funciona localmente
 db.produtos
   .updateMany({}, { $rename: { descricao: "descricaoSite" } });
+
+db.produtos
+  .find({}, { _id: 0, nome: 1, descricaoSite: 1 });
--- challenges/desafio30.js
+++ challenges/desafio30.js
@@ -1,3 +1,5 @@
-// funciona localmente
 db.produtos
-  .updateMany({ nome: "Big Mac" }, { $unset: { curtidas: "" } });
+  .updateOne({ nome: "Big Mac" }, { $unset: { curtidas: "" } });
+
+db.produtos
+  .find({}, { _id: 0, nome: 1, curtidas: 1 });
